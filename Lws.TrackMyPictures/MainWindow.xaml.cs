cb41e834ee9f19560dfaa0b9095b64d2b22c433e
RomanFedorvoskiy <likethisnick@gmail.com>
2016-08-20 20:15:30 +0300

    progress

--- Lws.TrackMyPictures/MainWindow.xaml.cs
+++ Lws.TrackMyPictures/MainWindow.xaml.cs
@@ -1,28 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
+using System.Net;
 
 namespace Lws.TrackMyPictures
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            MainMap.IgnoreMarkerOnMouseWheel = true;
+            MainMap.ShowCenter = false;
+            MainMap.DragButton = System.Windows.Input.MouseButton.Left;
+            MainMap.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionWithoutCenter;
+            MainMap.IgnoreMarkerOnMouseWheel = true;
+            MainMap.ShowCenter = false;
+            MainMap.MapProvider = GMapProviders.GoogleMap;
+            GeoCoderStatusCode status = GeoCoderStatusCode.Unknow;
+            PointLatLng? pos = GMapProviders.GoogleMap.GetPoint("Ukraine, Kyiv", out status);
+            MainMap.Position = new PointLatLng(pos.Value.Lat, pos.Value.Lng);
+            MainMap.MinZoom = 0;
+            MainMap.MaxZoom = 24;
+            MainMap.Zoom = 9;
         }
+
+        private void btnLeftMenuShow_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbShowLeftMenu", btnLeftMenuHide, btnLeftMenuShow, pnlLeftMenu);
+        }
+
+        private void btnLeftMenuHide_Click(object sender, RoutedEventArgs e)
+        {
+            ShowHideMenu("sbHideLeftMenu", btnLeftMenuHide, btnLeftMenuShow, pnlLeftMenu);
+        }
+
+        private void ShowHideMenu(string Storyboard, Button btnHide, Button btnShow, StackPanel pnl)
+        {
+            Storyboard sb = Resources[Storyboard] as Storyboard;
+            sb.Begin(pnl);
+
+            if (Storyboard.Contains("Show"))
+            {
+                btnHide.Visibility = System.Windows.Visibility.Visible;
+                btnShow.Visibility = System.Windows.Visibility.Hidden;
+            }
+            else if (Storyboard.Contains("Hide"))
+            {
+                btnHide.Visibility = System.Windows.Visibility.Hidden;
+                btnShow.Visibility = System.Windows.Visibility.Visible;
+            }
+        }
+
+        
     }
 }
